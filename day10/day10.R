64a6409c8a721249fbb9bacaee5bb88e40d7af8b
Matt Onimus <mattonimus@gmail.com>
2020-12-10 17:34:41 -0500

    finished day10

--- day10/day10.R
+++ day10/day10.R
@@ -1,7 +1,25 @@
 library(tidyverse)
 
 day10 <- tibble(x = c(as.numeric(read_lines('day10/day10.txt')),0))
-day10Ex <- tibble(x =c(as.numeric(read_lines('day10/day10Ex1.txt')),0))
+# day10Ex <- tibble(x =c(as.numeric(read_lines('day10/day10Ex1.txt')),0))
+# 
+# part1ex <- c(16,
+#              10,
+#              15,
+#              5,
+#              1,
+#              11,
+#              7,
+#              19,
+#              6,
+#              12,
+#              4,22)
+# 
+# ex1Sorted <- sort(part1ex)
+# 
+# ex1Sorted
+
+
 
 #part 1
 
@@ -14,18 +32,46 @@ table(day10Pt1$diff)
 
 # part 2
 
-day10Pt2 <- read_lines('day10/day10.txt') %>% 
+#some weird math to figure out if that path works?
+num <- c(1:175)
 
-totalCount <- 0
+#some weird math to figure out if that path works?
+num <- c(1:max(day10Pt1$x))
 
-part2 <- function(dataVec) {
+part2 <- tibble(num = num) %>% 
+  full_join(day10Pt1, by = c('num' = 'x')) %>% 
+  mutate(diff = if_else(is.na(diff), 0, diff))
+
+
+diff <- part2$diff
+
+totalCount <- c(1,2,3,7) # need to seed the function with the beginning of the cumsum vector
+
+part2Func <- function(diff, startingCount) {
   
-  for(i in 1:length(dataVec)) {
-    
-    
+  totalCount <- startingCount
+  
+  for(i in 5:length(diff)) {
+    #print(diff[[i]])
+    if(diff[[i]] == 0) {
+      totalCount <- c(totalCount, 0)
+      #print(totalCount)
+    } else {
+      
+      toAdd <- totalCount[[(i-1)]] + totalCount[[(i-2)]] + totalCount[[(i-3)]]
+      #print(toAdd)
+      
+      totalCount <- c(totalCount, toAdd)
+      #print(totalCount)
+    }
     
   }
   
+  return(totalCount[[(length(diff)-1)]])
   
 }
 
+
+
+
+print(part2Func(diff, totalCount), digits = 22)
